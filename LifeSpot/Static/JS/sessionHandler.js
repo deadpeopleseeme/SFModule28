e8f76ba3c7e4c1101a25ae13729aad6fdec990de
Eddy <volkikushaytnogi@gmail.com>
2024-08-25 14:24:00 +0200

    29.3 works properly but coded differently

--- LifeSpot/Static/JS/sessionHandler.js
+++ LifeSpot/Static/JS/sessionHandler.js
@@ -1,29 +1,43 @@
-function handleSession()
-{
+window.onload = function () {
+    console.log('sessionHandler.js loaded');
+
+    // Создадим и обработаем сессию
+    let session = handleSession();
+
+    // Логируем сессию в консоль
+    sessionLog(session);
+};
+
+function handleSession() {
     // создадим объект Map для хранения сессии
     let session = new Map();
-    // Сохраним UserAgent
-    session.set("userAgent", window.navigator.userAgent)
 
+    // Сохраним UserAgent
+    session.set("userAgent", window.navigator.userAgent);
 
     // Запросим возраст пользователя и тоже сохраним
-    session.set("age", prompt("Пожалуйста, введите ваш возраст?"))
+    let age = prompt("Пожалуйста, введите ваш возраст?");
 
     // Проверка на возраст и сохранение сессии
-    if (session.get("age") >= 18) {
+    if (age >= 18)
+    {
         let startDate = new Date().toLocaleString();
-
         alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + startDate);
-        session.set("startDate", startDate)
+        session.set("startDate", startDate);
     }
-    else {
+    else
+    {
         alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
-        window.location.href = "http://www.google.com"
-        a = true + 20 + "name"
+        window.location.href = "http://www.google.com";
     }
 
+    session.set("age", age);
     return session;
 }
 
-
-
+// Логирование сессии (объявлено через function expression)
+let sessionLog = function (session) {
+    for (let [key, value] of session) {
+        console.log(`${key}: ${value}`);
+    }
+};
